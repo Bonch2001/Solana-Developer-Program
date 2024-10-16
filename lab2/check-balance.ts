91bc8a3b808f405f334cf1133b0ae753b4bba787
Alin Bonciu <bonciu_alin@yahoo.com>
2024-10-16 23:53:50 +0300

    lab2

---
+++ lab2/check-balance.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import {
+  Connection, LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+
+import { airdropIfRequired } from "@solana-developers/helpers";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+console.log("Connected to devnet", connection.rpcEndpoint);
+
+const myPubkey = new PublicKey("5JwW4uYcpMues4RjzCf6wLcrj8PTD6bQcn6MBWXQ4GTE");
+
+const balanceInLamports = await connection.getBalance(myPubkey);
+
+console.log("Current balance in lamports:", balanceInLamports);
+
+console.log("Airdropping 1 SOL...");
+
+await airdropIfRequired(connection, myPubkey, 1 * LAMPORTS_PER_SOL, 2 * LAMPORTS_PER_SOL);
+
+console.log("Done! Airdropped 1 SOL");
+
+const balanceInLamports2 = await connection.getBalance(myPubkey);
+
+console.log("Final balance in lamports:", balanceInLamports2);
